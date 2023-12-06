83d3c8de8b26154608a751fbbedc3f242b2dab89
kawatech002 <kawanjiku94@gmail.com>
2023-12-06 04:35:36 +0300

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    
+    ''' If length of array is 1, return the item. If the length is 2, return 'item 1 and item 2' else, join the items to a string, '''
+    if(len(items) == 1):
+        return items[0]
+    elif(len(items) == 2):
+        return f"{items[0]} and {items[1]}"
+    
+    
+    else:
+        join= items[-1]
+        return(f"{', '.join(items[0:-1])}, and {join}")
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
